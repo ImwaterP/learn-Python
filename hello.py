0749eca2cab8b2959e5afaea6aa0f8930e2ad9c1
ImwaterP <787019817@qq.com>
2019-02-20 20:37:36 +0800

    This is my first time to learn Python!

---
+++ hello.py
@@ -0,0 +1,179 @@
+﻿#!/usr/bin/env python3
+#  _*_ coding: utf_8 _*_
+
+
+
+# sum = 0
+# a = list(range(5))
+# for x in a:
+	# sum = sum + x
+# print(sum)
+
+# count = 1
+# while count > 0:#无限循环，可以用CTRL + C 中止
+	# print('The count is:%d' % count)
+	# count = count + 1
+# else :
+	# print('count is > 10')
+
+# print('goodbey')
+
+
+# s = input('birth = ')
+# birth = int(s)
+# if birth < 2000:
+	# print('00前')
+# else:
+	# print('00后')
+
+# height = input('enter your height= ')
+# weight = input('enter your weight= ')
+# height = float(height)
+# weight = float(weight)
+# bmi = weight /(height * height)
+# print('bim = %d' % bmi)
+# if bmi < 18.5:
+	# print('体重过轻')
+# elif bmi >= 18.5 and bmi < 25:
+	# print('正常')
+# elif bmi >= 25 and bmi < 28:
+	# print('过重')
+# elif bmi >= 28 and bmi < 32:
+	# print('肥胖')
+# else:
+	# print('过度肥胖')
+
+# age = 18
+# if age > 18:
+	# print('your age is',age)
+	# print('adult')
+# elif age >= 6:
+	# print('teenage')
+# else age:
+	# print('your age is',age)
+	# print('kid')
+
+# L = [
+# ['Apple','Goole','Microsoft'],
+# ['Java','Python','Ruby','PHP'],
+# ['Adam','Bart','Lisa']
+# ]
+# print(L[0][0])
+# print(L[2][2])
+# print(L[1][1])
+
+
+# t = ('a','b',['A','B'])
+# print(t[0])
+# print(t[2][0])
+# print(t[2][1])
+
+# t[2][0] = 'X'
+# t[2][1] = 'Y'
+# print(t[2][0])
+# print(t[2][1])
+
+
+# t1 = (4)
+# t2 = (4,)
+# print(t1,t2)
+# print(len(t2))
+
+# classmates = ('one', 'two', 'three')
+# print(classmates)
+
+
+# p = [1,2]
+# s = [99,100,p,111]
+# print(s)
+# print(s[2][1])
+
+# classmates = ['one', 'two', 
+# 'three']
+# print(len(classmates))
+# print(classmates[0], classmates[1], classmates[-1])
+# classmates.append('other')#一次只能附一个值
+# print(classmates[-1])
+# classmates.insert(0,'jack')
+# classmates.pop()
+# print(classmates)
+# classmates.pop(1)
+# print(classmates)
+# classmates[0] = 'jenny'
+# print(classmates)
+
+# print('ABC'.encode('ascii'))
+# 'ABC'.encode('utf_8')
+# '中文'.encode('utf_8')
+# print('中文'.encode('utf_8'))
+
+# print('\n')
+
+# b'ABC'.decode('ascii')
+# print(b'ABC'.decode('ascii'))
+# b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf_8')
+# print(b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf_8'))
+
+
+
+# name = input('enter your name: ')
+# print('hello,%s'%name)
+# age = 22
+# money = 8000.50
+# print('your age is %s\nAnd you earn %5.2f everymonth.'%(age, money))
+
+# print('%0d - %02d' % (3, 1))
+# print('%.2f' % 3.1415926)
+# print('%5.2f' % 3.1415926)
+# print('%s' % 3.1415926)
+# print('%%6')
+
+# s1 = 72
+# s2 = 85
+# r = (s2 - s1) / s1 * 100
+# print('Hello,小明，你的成绩提升了%.1f%%' % r)
+
+
+
+# # a = '我爱你'
+# # print('中文测试')
+# # print(a)
+
+
+# print ('''n = 123
+# f = 456.789
+# s1 = Hello, world
+# s2 = Hello, 'Adam'
+# s3 = Hello,''Bart''
+# s4 = Hello,
+# Lisa!''')
+# n = 123
+# print('n =',n)
+# f = 456.789
+# print('f =',f)
+# print('s1 = \'Hello world\'')
+# print('s2 = \'Hello,\\\'Adam\\\'\'')
+# print('s3 = r\'Hello,"Bart"\'')
+# print('''s4 = r\'\'\'Hello,
+# Lisa!\'\'\'''')
+# print("s4 = r'''hello,\nLisa!'''")
+
+# \n
+# print('''line1
+# line2
+# line3''')
+
+# print('line'
+# ,'line'
+# ,'line')
+
+# print(r'''hello,\n
+# world.''')
+
+# name = input('enter your name: ')
+
+# print(r'''hello,
+# name.''',name)
+
+
+
